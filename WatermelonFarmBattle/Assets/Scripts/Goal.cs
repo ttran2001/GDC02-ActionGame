f06dc840a598a28d0e0d0e826b9274a94ce43e20
Tyler Tuan Tran <tylertran161@gmail.com>
2023-04-25 12:56:53 -0500

    added scripts

--- WatermelonFarmBattle/Assets/Scripts/Goal.cs
+++ WatermelonFarmBattle/Assets/Scripts/Goal.cs
@@ -5,18 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
-    static public bool goalMet = false; 
 
+	[Header("Set Dynamically")]
+	public int goalHealth = 5;
 
-    void OnTriggerEnter(Collider other){
-    // If the Cat Asset collides with the goal, it will set the goalMet to true and change color
-        if(other.gameObject.tag == "Cat"){
-            Goal.goalMet = true; 
-            Material mat = GetComponent<Renderer>().material;
-            Color c = mat.color; 
-            c.a=1; 
-            mat.color=c;
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1); // Load the next scene of the game. Last screen it will load is the Game Over scene
-        }
-    }
+	VoidOnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "animal")
+		{
+			goalHealth--;
+			if (goalHealth <= 0)
+			{
+				SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+			}
+		}
+	}
 }
